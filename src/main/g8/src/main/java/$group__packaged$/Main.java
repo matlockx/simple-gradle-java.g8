013902372d8a73035d96c033cdcc1ebdaa0ae927
Martin Jöhren <m.joehren@xplosion.de>
2012-09-17 10:16:46 +0200

    inital commit

---
+++ src/main/g8/src/main/java/$group__packaged$/Main.java
@@ -0,0 +1,12 @@
+package $group__packaged;format="lower"$;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}

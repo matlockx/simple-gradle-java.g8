c6576d59272975adb6ff13ec61b37985d7901a47
Martin Jöhren <m.joehren@xplosion.de>
2012-09-17 15:39:52 +0200

    escape $

--- src/main/g8/src/bdd-test/java/$group__packaged$/ManageUsersTest.java
+++ src/main/g8/src/bdd-test/java/$group__packaged$/ManageUsersTest.java
@@ -7,32 +7,32 @@ import cucumber.runtime.PendingException;
 import cucumber.table.DataTable;
 
 public class ManageUsersTest {
-    @Given("^the following user records$")
+    @Given("^the following user records\$")
     public void the_following_user_records(DataTable arg1) throws Throwable {
         // Express the Regexp above with the code you wish you had
         // For automatic conversion, change DataTable to List<YourType>
         throw new PendingException();
     }
 
-    @Given("^I am logged in as \"([^\"]*)\" with password \"([^\"]*)\"$")
+    @Given("^I am logged in as \"([^\"]*)\" with password \"([^\"]*)\"\$")
     public void I_am_logged_in_as_with_password(String arg1, String arg2) throws Throwable {
         // Express the Regexp above with the code you wish you had
         throw new PendingException();
     }
 
-    @When("^I visit profile for \"([^\"]*)\"$")
+    @When("^I visit profile for \"([^\"]*)\"\$")
     public void I_visit_profile_for(String arg1) throws Throwable {
         // Express the Regexp above with the code you wish you had
         throw new PendingException();
     }
 
-    @Then("^I should see \"([^\"]*)\"$")
+    @Then("^I should see \"([^\"]*)\"\$")
     public void I_should_see(String arg1) throws Throwable {
         // Express the Regexp above with the code you wish you had
         throw new PendingException();
     }
 
-    @Then("^I should not see \"([^\"]*)\"$")
+    @Then("^I should not see \"([^\"]*)\"\$")
     public void I_should_not_see(String arg1) throws Throwable {
         // Express the Regexp above with the code you wish you had
         throw new PendingException();
